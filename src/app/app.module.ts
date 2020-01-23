ce4ab028a15d0809a13eebaf904ace3964c7d0eb
Marcelo dos Santos Alves <MarceloAlves@caesb.df.gov.br>
2020-01-23 12:44:38 -0300

    13.8. Validando controles de formulário com PrimeNG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
@@ -35,6 +36,8 @@ import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.compo
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+
     InputTextModule,
     ButtonModule,
     DataTableModule,
