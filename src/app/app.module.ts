ce6586b22bd34f329e88974760f6cfe0151a50bd
Marcelo dos Santos Alves <MarceloAlves@caesb.df.gov.br>
2020-02-03 12:49:56 -0300

    14.11. Desafio: criando o Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { CoreModule } from './core/core.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
     ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
 
+    CoreModule,
     LancamentosModule,
     PessoasModule
   ],
