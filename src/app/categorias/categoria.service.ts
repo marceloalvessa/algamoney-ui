cef29b3216ce66a0b252338546769a53361ec429
Marcelo dos Santos Alves <MarceloAlves@caesb.df.gov.br>
2020-03-10 17:16:34 -0300

    20.1. Configurando a aplicação com environment do Angular CLI

--- src/app/categorias/categoria.service.ts
+++ src/app/categorias/categoria.service.ts
@@ -1,13 +1,16 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 
+import { environment } from './../../environments/environment';
 import { AuthHttp } from 'angular2-jwt';
 @Injectable()
 export class CategoriaService {
 
-  categoriasUrl = 'http://localhost:8080/categorias';
+  categoriasUrl : string;
 
-  constructor(private http: AuthHttp) { }
+  constructor(private http: AuthHttp) {
+    this.categoriasUrl = `${environment.apiUrl}/categorias`;
+  }
 
   listarTodas(): Promise<any> {
     return this.http.get(this.categoriasUrl)
