475653e917d473376ef4379af5e7b6a43eea5725
Marcelo dos Santos Alves <MarceloAlves@caesb.df.gov.br>
2020-03-10 12:40:10 -0300

    19.8. Exibindo o nome do usuário logado

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from './../../seguranca/auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
   }
