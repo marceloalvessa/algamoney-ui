0b5a47860c2b540f7aa4d5717bb765dfb3e8e729
Marcelo dos Santos Alves <MarceloAlves@caesb.df.gov.br>
2020-03-16 12:42:04 -0300

    21.5. Exercício: utilizando TurboTable na pesquisa de pessoas

--- src/app/pessoas/pessoas.module.ts
+++ src/app/pessoas/pessoas.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InputMaskModule } from 'primeng/inputmask';
-import { DataTableModule } from 'primeng/datatable';
+import { TableModule  } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { TooltipModule } from 'primeng/tooltip';
@@ -20,7 +20,7 @@ import { SharedModule } from './../shared/shared.module';
 
     InputTextModule,
     ButtonModule,
-    DataTableModule,
+    TableModule,
     TooltipModule,
     InputMaskModule,
 
