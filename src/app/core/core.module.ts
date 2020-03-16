209df04ae4f2d1ea2c2121bd1d8ed980fb1275ef
Marcelo dos Santos Alves <MarceloAlves@caesb.df.gov.br>
2020-03-16 12:18:00 -0300

    21.2. Corrigindo problemas com Locale

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,8 @@
 import { RouterModule } from '@angular/router';
 import { NgModule, LOCALE_ID } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Title } from '@angular/platform-browser';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { ToastyModule } from 'ng2-toasty';
 import { JwtHelper } from 'angular2-jwt';
@@ -16,6 +17,8 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ErrorHandlerService } from './error-handler.service';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada.component';
 import { AuthService } from './../seguranca/auth.service';
+
+registerLocaleData(localePt);
 
 @NgModule({
   imports: [
